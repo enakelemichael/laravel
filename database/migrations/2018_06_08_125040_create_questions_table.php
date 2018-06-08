b3e3b7411f8dfb4f4ebea5d35b0f891ce8340c34
Enakelemichael <enakelemichael@yahoo.com>
2018-06-08 15:35:03 +0200

    done

---
+++ database/migrations/2018_06_08_125040_create_questions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->nullable();
+            $table->String('title', 255)->nullable();
+            $table->text('text')->nullable();
+            $table->timestamps();//creates created_at and update_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
